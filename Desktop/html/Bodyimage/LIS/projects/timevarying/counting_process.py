e19428bdd6676f58000ee5c95a2ace67c4c4a4ff
gquinn <equinn1@stonehill.edu>
2016-06-30 08:02:37 -0400

    adding cp python

--- Desktop/html/Bodyimage/LIS/projects/timevarying/counting_process.py
+++ Desktop/html/Bodyimage/LIS/projects/timevarying/counting_process.py
@@ -6,69 +6,76 @@ Created on Thu Jun 16 09:47:26 2016
 """
 import pickle
 from counting_process_classes import *
-        
-fp = open('../fouryear/data/STATE_BDD.txt','r')   #read state changes
 
 subjects={}                                       #subject dictionary
-linect=0
-for line in fp:                                   #loop through line by line
-    linect=linect+1
-    if("SS:" in line):                            #first entry for this S
-        wds = line.split()
-        SS=wds[0].split(":")[1]                   #subject number
-        scd102=wds[2]                             #BDD intake status
-        scd104=wds[4]
-        scd106=wds[6]
+
+for dis in ['BDD','MDD','OCD','SOC']:
+    fname="data/STATE_"+dis+".txt"
+
+    fp = open(fname,'r')   #read state changes
+
+    for line in fp:                              #loop through line by line
+        if("SS:" in line):                            #first entry for this S
+            wds = line.split()
+            SS=wds[0].split(":")[1]                   #subject number
+            initline=line
 
         
-    if("ID" in line):                             #ID line
-        wl=line.split("=")
-        if(len(wl)>1):                            #skip if no ID
-            ID=wl[1]                              #save ID if there is one
+        if("ID" in line):                             #ID line
+            wl=line.split("=")
+            if(len(wl)>1):                            #skip if no ID
+                if(ID in subjects.keys()):            #ID already present?
+                    newsub=subjects[ID]
+                else:
+                    ID=wl[1]                              #save ID if there is one
+                    newsub=subj(ID,SS)                    #call subject constructor
+                    subjects[ID]=newsub
             
-    if("Initial" in line):                        #save initial status
-        init_state=line.split()[2]
-        newsub=subj(ID,SS,scd102,scd104,scd106)
-        cpentries=[]
-        cpent=cpentry(0,None,init_state)
-        cpentries.append(cpent)
-            
-    if("Max" in line):
-        maxwk=line.split()[2]
-        newsub.set_maxwk(maxwk)
-        lastone=len(cpentries)-1
-        lastcp=cpentries[lastone]
-        lastcp.set_end(maxwk)
-        newsub.set_cpentries(cpentries)
-        if(ID != None):
-            subjects[ID]=newsub
-
-        print(ID)
+        if("Initial" in line):                        #save initial status
+            init_state=line.split()[2]
+            newsub.set_intake_state(init_state,dis)
+            cpentries=[]
+            newcp=cpentry(0,None,init_state)
+            cpentries.append(newcp)
+            newsub.set_cpentries(cpentries,dis)
         
-    if("STATE CHANGE" in line):
-        wds=line.split()
-        from_state=wds[3]
-        to_state=wds[5]
-        week=wds[8]
-        imax=len(cpentries)-1
-        cpe = cpentries[imax]
-        cpe.set_end(week)
-        newcp=cpentry(week,None,to_state)
-        print(len(cpentries))
-        cpentries.append(newcp)
-        print(len(cpentries))
-        print(week)
+            
+        if("Max" in line):
+            maxtime=line.split()[2]
+            newsub.set_maxtime(maxtime)
+            cpentries=newsub.get_cpentries(dis)
+            lastone=len(cpentries)-1
+            lastcp=cpentries[lastone]
+            lastcp.set_end(maxtime)
+            newsub.set_cpentries(cpentries,dis)
+
+        if("STATE CHANGE" in line):
+            wds=line.split()
+            from_state=wds[3]
+            to_state=wds[5]
+            week=wds[8]
+            newcp=cpentry(week,None,to_state)
+            cpentries=newsub.get_cpentries(dis)
+            imax=len(cpentries)-1
+            cpe = cpentries[imax]
+            cpe.set_end(week)
+            cpentries.append(newcp)
+            newsub.set_cpentries(cpentries,dis)
 
 print("Starting list")
 for key in subjects:
-    print(subjects[key].get_ID())
-    cpentries=subjects[key].get_cpentries()
-    for i in range(0,len(cpentries)):
-        cpe = cpentries[i]
-        start=cpe.get_start()
-        end=cpe.get_end()
-        state=cpe.get_state()
-        print "%d %s %s %s" % (i, start, end, state)
+#    print(subjects[key].get_ID())
+    cpdict=subjects[key].get_cpdict()
+    for dis in cpdict.keys():
+        cpentries=cpdict[dis]
+        for i in range(0,len(cpentries)):
+            cpe = cpentries[i]
+            start=cpe.get_start()
+            end=cpe.get_end()
+            state=cpe.get_state()
+#            print "%d %s %s %s" % (i, start, end, state)
+
+        print(dis)
         
-pf=open('bdd_state_python.obj','w')      
+pf=open('bdd_state_python.obj','wb')      
 pickle.dump(subjects,pf,pickle.HIGHEST_PROTOCOL)
--- Desktop/html/Bodyimage/LIS/projects/timevarying/counting_process_classes.py
+++ Desktop/html/Bodyimage/LIS/projects/timevarying/counting_process_classes.py
@@ -4,57 +4,116 @@ Created on Sat Jun 18 07:40:18 2016
 
 @author: gquinn
 """
+class cpobj:                      #counting process object class
+    def cp_add(self,time,flag):                 #add a cp entry
+        for i in range(0,len(self.start)):      #find insertion point
+            if(self.start[i]<=time):            #start must be <= time
+                if(self.end[i] >= time):        #end must be >= time
+                    temp_end=self.end[i]        #copy current end time
+                    temp_flag=self.flag[i]      #copy current flag
+                    temp_cov=self.covariates[i].copy() #copy covariates
+                    self.end[i]=time            #set end time=time
+                    self.flag[i]=flag           #set flag=input flag
+                    self.start.insert(i,time)   #new entry with start=time
+                    self.end.insert(i,temp_end) #copy old end time
+                    self.flag.insert(i,temp_flag) #copy old flag
+                    self.covariates.insert(i,temp_cov) #copy old covariates
+        return
+    def get_covariates(self):                   #return covariates dictionary
+        return self.covariates
+        
+    def set_covariates(self,start,end,key,value): #set covariates value
+        instart=False                             #check if start time in list
+        for i in range(0,len(self.start)):        #loop through start
+            if(self.start[i]==start):             #set instart=True if match
+                instart=True
+        if(instart==False):                       #if it's not there
+            self.cp_add(start,4)                  # add it
+        inend=False                               #check if end time in list
+        for i in range(0,len(self.end)):          #loop through end times
+            if(self.end[i]==end):                 #set inend=true if match
+                inend=True
+        if(inend==False):                         #if it's not there
+            self.cp_add(end,4)                    # add it
+        for i in range(0,len(self.start)):        #now loop through cp entries
+            if (self.start[i]<=start & self.end[i]>=end):  #if start/end match
+                self.covariates[i][key]=value     #set covariates value
+        return
+        
+    def print_cp(self):
+        for i in range(0,len(self.start)):        #print cp entry
+            print "%d %d %d" % (self.start[i], self.end[i], self.flag[i])
+            print(self.covariates[i])
+        return        
+        
+    def __init__(self,start,end):                 #cpobj constructor
+        self.start=[]
+        self.end=[]
+        self.flag=[]
+        self.covariates=[]
+        self.start.insert(0,start)
+        self.end.insert(0,end)
+        self.flag.insert(0,2)
+        self.covariates.insert(0,{})
+        return
 
-
-class cpentry:
-    def get_start(self):
+class cpentry:           #counting process entry class
+    def get_start(self):                    #return start time  
         return self.start
-    def set_start(self,start):
+    def set_start(self,start):              #set start time
         self.start=start
         return
-    def get_end(self):
+    def get_end(self):                      #return end time
         return self.end
-    def set_end(self,end):
+    def set_end(self,end):                  #set end time
         self.end=end
         return
-    def get_state(self):
-        return self.state
-    def set_state(self,state):
+    def get_state(self):                    #get state
+        return self.state              
+    def set_state(self,state):              #set state
         self.state=state
         return
-    def __init__(self,start,end,state):
+    def __init__(self,start,end,state):     #cpentry constructor
         self.start=start
         self.end=end
         self.state=state
         return
 
-class subj:
-    def get_init(self):
-        return self.init
-    def set_init(self,init,dis):
-        self.init[dis]=init
+class subj:                                 #subject class
+    def get_intake_states(self):               #get intake state 
+        return self.intake_state
+    def get_intake_state(self,dis):               #get intake state 
+        return self.intake_state[dis]
+    def set_intake_state(self,init,dis):      #set intake state
+        self.intake_state[dis]=init
         return
-    def get_cpentries(self):
-        return self.cpentries
-    def set_cpentries(self,cpentries):
-        self.cpentries=cpentries
+    def get_cpdict(self):                     #return cp entries dictionary
+        return self.cpdict 
+    def get_cpentries(self,dis):              #return cp entries
+        return self.cpdict[dis]
+    def set_cpentries(self,cpentries,dis):       #set cpentries
+        self.cpdict[dis]=cpentries
         return
-    def get_ID(self):
+    def get_ID(self):                         #return ID
         return self.ID
-    def set_ID(self,ID):
+    def set_ID(self,ID):                      #set ID
         self.ID=ID
         return
-    def get_maxwk(self):
-        return self.maxwk
-    def set_maxwk(self,maxwk):
-        self.maxwk=maxwk
+    def get_initline(self):                   #return initline dictionary
+        return self.initline
+    def set_initline(self,dis,iline):         #return initline for disorder
+        self.initline[dis]=iline
         return
-    def __init__(self,ID,SS,scd102,scd104,scd106):
-        self.ID=ID
-        self.scd102=scd102
-        self.scd104=scd104
-        self.scd106=scd106
-        self.SS=SS
-        self.maxwk=None
-        self.cpentries=[]
+    def get_time(self):                      #return max time
+        return self.maxtime
+    def set_maxtime(self,maxtime):             #set max time
+        self.maxwk=maxtime
+        return
+    def __init__(self,ID,SS):      #subject constructor
+        self.ID=ID                          #subject ID
+        self.initline={}                    #init line images
+        self.SS=SS                          #SS number
+        self.maxtime=None                   #max time for subject
+        self.intake_state={}                #intake state dictionary
+        self.cpdict={}                      #cp entries dictionary
         return
